64875a0822ab6f0cb9f8fe15a0e7c00ebd0f0b89
Joseph Kulig <kulandthegang@gmail.com>
2024-07-23 19:49:24 -0400

    Initial code

---
+++ env.sh
@@ -0,0 +1,10 @@
+export ALT_BOOTDIR=/Library/Java/JavaVirtualMachines/zulu-7.jdk/Contents/Home
+export LANG=C
+export ALT_CUPS_HEADERS_PATH=/opt/homebrew/Cellar/cups/2.4.10/include
+export ALT_FREETYPE_HEADERS_PATH=/opt/homebrew/opt/freetype2/include
+export ALT_FREETYPE_LIB_PATH=/opt/homebrew/lib
+export ANT_HOME=/opt/homebrew/opt/ant@1.9/libexec
+export ALT_JDK_IMPORT_PATH=/Library/Java/JavaVirtualMachines/zulu-7.jdk/Contents/Home
+export ARCH=arm64
+#export EXTRA_CFLAGS="-Wnoerror,-Wdeprecated-declarations -Wnoerror,-Wparentheses-equality"
+#export PATH=/Users/joe/local/apache-ant-1.7.1/bin:$PATH
